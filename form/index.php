455af9968210b4355e29fc161d5d1fcccddff12d
HsuTing <hsuting0106@gmail.com>
2015-06-03 15:03:25 +0800

    basic form

---
+++ form/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv ="Content-Type" content="text/html;charset=utf-8"/>
+
+		<link type="text/css" rel="stylesheet" href="css/set.css"/>
+	</head>
+
+	<body>
+		<form action = "final.php" method = "post">
+			<h1>大學生活體驗營 選課系統</h1>
+			<h3>姓名：徐鼎翔&nbsp;&nbsp;&nbsp;&nbsp;類組：二三類組</h3>
+			<div id = "type"><div id = "2"></div></div>
+
+			<div id = "form"></div>
+		</form>
+	</body>
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</html>
